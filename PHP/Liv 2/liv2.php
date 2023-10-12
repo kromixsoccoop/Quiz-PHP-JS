4d54ff90ef7a1185af162671bf5ac134356f84c4
KrisTwinn <djchris.cg@gmail.com>
2023-10-12 11:58:26 +0200

    Update liv2.php

--- PHP/Liv 2/liv2.php
+++ PHP/Liv 2/liv2.php
@@ -19,31 +19,11 @@ Array
 )
 */
 
+$pattern = '/\b\d{10}\b/';
+$numeri = [];
+$persona = implode(", ", $persone);
 
-/*-------------------PRIMA SOLUZIONE----------------------
-$stringa = implode(' : ', $persone);  //prendo gli elem di persone e li salvo tutti in una stringa separati da ' : '
-
-$parole = explode(':', $stringa);   //prendo la stringa e la salvo nell'array parole, ogni elemento è separato da ':'
-
-for($i=3; $i<=count($parole)-1; $i+=4){ //itera a partire da 3 e incrementa di 4 fino a quando non raggiunge la lunghezza dell'array-1
-    echo "\n", $parole[$i]; //stampo il numero di telefono che è salvato nelle posizioni i+4 con i>=3
+if (preg_match_all($pattern, $persona, $matches)) {
+    $numeri = array_merge($numeri, $matches[0]);
 }
-*/
-
-$telefoni = [];
-
-foreach ($persone as $persona) {    //scorro l'array $persone ed ogni elemento trovato lo salvo in $persona
-    
-    $dettagli = explode(', ', $persona);    // divido la stringa di persona in un array utilizzando ', ' come delimitatore
-
-    foreach ($dettagli as $dettaglio) { // Itera attraverso i dettagli per trovare il numero di telefono
-        if (strpos($dettaglio, 'Telefono:') !== false) {    //se trova la posizione dove legge 'Telefono:'
-            
-            $numeroTelefono = str_replace('Telefono: ', '', $dettaglio);    // estraggo numero di telefono rimuovendo 'Telefono:' e spazi
-            $telefoni[] = $numeroTelefono;  //lo salvo nell'array creato all'inizio
-            break; // esce una volta trovato il numero
-        }
-    }
-}
-
-print_r($telefoni); //stampo l'array
+print_r($numeri);
